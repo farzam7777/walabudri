71a168874bdeded4265e4773ba153d70c680e3ed
farzam <l144211@lhr.nu.edu.pk>
2017-07-23 19:02:17 +0500

    geocoder issue resolved.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,22 +1,17 @@
 Geocoder.configure(
-  Geocoding options
-  timeout: 20,                 # geocoding service timeout (secs)
-  lookup: :google,            # name of geocoding service (symbol)
-  ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
-  language: :en,              # ISO-639 language code
-  use_https: false,           # use HTTPS for lookup requests? (if supported)
-  http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-  https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: nil,               # API key for geocoding service
-  cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+  timeout: 20, 
+  lookup: :google, 
+  ip_lookup: :freegeoip,
+  language: :en,
+  use_https: false,
+  http_proxy: nil,
+  https_proxy: nil,
+  api_key: nil,
+  cache: nil,
+  cache_prefix: 'geocoder:',
 
-  Exceptions that should not be rescued by default
-  (if you want to implement custom error handling);
-  supports SocketError and Timeout::Error
   always_raise: [],
 
-  Calculation options
-  units: :mi,                 # :km for kilometers or :mi for miles
-  distances: :linear          # :spherical or :linear
+  units: :mi,
+  distances: :linear
 )
