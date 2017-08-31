5b0de352a5179fc6c2299a7a664969a823eabdb3
farzam <l144211@lhr.nu.edu.pk>
2017-08-31 17:38:11 +0500

    Assets

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,6 +1,5 @@
+var CKEDITOR_BASEPATH = '/assets/ckeditor/';
 //= require active_admin/base
 //= require_self
 //= require ckeditor/ckeditor
 //= require ckeditor/init
-
-var CKEDITOR_BASEPATH = 'http://assets.walabudri.herokuapp.com/assets/ckeditor/';
