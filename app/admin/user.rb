9e60f9a3470d60705df822b6812f3416a71de3a8
farzam7777 <l144211@lhr.nu.edu.pk>
2017-11-06 20:45:11 +0500

    assets

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,46 @@
 ActiveAdmin.register User do
-  
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
-permit_params :email, :first_name, :image, :contact, :id, :middle_name, :last_name
+	permit_params :email, :first_name, :image, :contact, :id, :middle_name, :family_name, :terms, :password, :password_confirmation
+
+	action_item :Approve, only: :show do 
+		link_to "Approve", Approve_admin_user_path(user), method: :put if !user.confirmed?
+	end	
+
+	member_action :Approve, method: :put do
+		user = User.find(params[:id])
+		user.skip_confirmation!
+		user.save
+		redirect_to [:admin, user], notice: 'User is Successfully Approved.'
+	end
+
+
+	index do
+	  selectable_column
+	  column :id
+	  column :email
+	  column :first_name
+	  column :middle_name
+	  column :family_name
+	  image_column :image, style: :thumb
+	  column :contact
+	  actions
+	end
+
+	form multipart: true do |f|
+	  f.inputs "User Details" do
+	  	f.input :email
+	  	f.input :password
+	  	f.input :password_confirmation
+	    f.input :first_name
+	    f.input :middle_name
+	    f.input :family_name
+	    f.input :contact
+	    f.input :terms, input_html: { value: 1 }
+	    f.input :image, :as => :file, :hint => f.object.image.present? \
+	    ? image_tag(f.object.image.url(:medium))
+	    : content_tag(:span, "no image yet") 
+	  end
+	  f.button :Submit
+	end
 
 end
