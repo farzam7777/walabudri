33ae4a3e29944c05042c50b870eddf6b778d7e07
farzam7777 <l144211@lhr.nu.edu.pk>
2017-11-06 22:57:02 +0500

    migration changed

--- db/migrate/20171106141910_add_super_admin_to_admin_users.rb
+++ db/migrate/20171106141910_add_super_admin_to_admin_users.rb
@@ -1,5 +1,5 @@
 class AddSuperAdminToAdminUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :admin_users, :super_admin, :boolean, default: 0
+    add_column :admin_users, :super_admin, :boolean, default: false
   end
 end
